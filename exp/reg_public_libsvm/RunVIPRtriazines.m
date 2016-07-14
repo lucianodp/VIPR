9aa35168911c8d742d2c283b02e7e4c88fe40eaa
Madalina Fiterau <ina_fiterau@yahoo.com>
2016-07-13 23:03:56 -0400

    Added triazines and year prediction

---
+++ exp/reg_public_libsvm/RunVIPRtriazines.m
@@ -0,0 +1,13 @@
+clear;
+load('triazines.mat');
+Solver = @SVMregressionSolver; % Solver takes X, Y as parameters
+Predict = @SVMregressionPredict;
+d_range = 3:3;
+P = 2; % number of projections to be selected
+ComputeLoss = @ComputeRegSqLossEst;  % compute local MSE for an estimator based on 3-nearest neighbors
+LossParams = 3; % the parameter k for the loss computation
+SelectionMethod = 'greedy';
+SelectionParams = 0;
+[VIPRmodel] = VIPRTrainModel(Solver, X, Y, d_range, P, ComputeLoss, LossParams, SelectionMethod, SelectionParams);
+[VIPRmodel] = VIPRaddFeatureNames(VIPRmodel, Features);
+[FigHandles] = VIPRPlotModel(VIPRmodel);
